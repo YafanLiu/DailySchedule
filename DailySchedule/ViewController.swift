4b8e7738d7a7d609cd0c3493f1168c636133e430
Yafan Liu <liu352@mcmaster.ca>
2015-09-27 23:25:59 -0400

    First Commit

--- DailySchedule/ViewController.swift
+++ DailySchedule/ViewController.swift
@@ -10,15 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var time: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let timestamp = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: .MediumStyle, timeStyle: .ShortStyle)
+        println(timestamp)
+        time.text = timestamp
+        timecheckings()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func timecheckings(){
+        let timestamp = NSDateFormatter.localizedStringFromDate(NSDate(), dateStyle: .MediumStyle, timeStyle: .ShortStyle)
+        let event2 = "Sep 27, 2015, 12:47 AM"
+        let event3 = "Sep 27, 2015, 13:00 AM"
+        if timestamp > event2  && timestamp < event3{
+            println("haha")
+        }
+        else{
+            println("NONONO")
+        }
+        let date = NSDate()
+        let calendar = NSCalendar.currentCalendar()
+        let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute | .CalendarUnitMonth | .CalendarUnitYear | .CalendarUnitDay, fromDate: date)
+        let hour = components.hour
+        let minutes = components.minute
+        let month = components.month
+        let year = components.year
+        let day = components.day
+        println(minutes)
+        println(hour)
+    }
+
 
 
 }
