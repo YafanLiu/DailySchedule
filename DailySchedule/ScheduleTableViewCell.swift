ea6d8f64fe242277bdecdae886ab08343a93307d
Yafan Liu <liu352@mcmaster.ca>
2015-09-29 00:23:07 -0400

    pass data

--- DailySchedule/ScheduleTableViewCell.swift
+++ DailySchedule/ScheduleTableViewCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ScheduleTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var courseTitle: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
